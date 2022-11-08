6d3df9d27df51a9fb61a4faf058e0579d5e9178b
GreenGrass <72069705+greengrass19000@users.noreply.github.com>
2022-11-08 20:01:01 +0700

    Create paths.ts

---
+++ frontend/src/paths.ts
@@ -0,0 +1,8 @@
+const paths = {
+  CustomerManagement: '/admin/customers',
+  FilmManagement: '/admin/films',
+  RoomManagement: '/admin/rooms',
+  TicketManagement: '/admin/tickets',
+};
+
+export default paths;
