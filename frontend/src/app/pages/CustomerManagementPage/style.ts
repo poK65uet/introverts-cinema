59847246c846f3b98439005111f4068c56091bba
GreenGrass <72069705+greengrass19000@users.noreply.github.com>
2022-11-08 19:54:22 +0700

    Merge branch
    Merge dashboard -> dev

---
+++ frontend/src/app/pages/CustomerManagementPage/style.ts
@@ -0,0 +1,12 @@
+import { Height } from '@mui/icons-material';
+import { makeStyles } from '@mui/styles';
+import { spacing } from '@mui/system';
+
+const useStyles = makeStyles(() => ({
+customerTable: {
+    height: 400, 
+    width: '80%',
+},
+}));
+
+export default useStyles;
---
+++ frontend/src/app/pages/RoomManagementPage/index.tsx
@@ -0,0 +1,11 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useGetMessage } from 'queries/message';
+
+export default function RoomManagementPage() {
+  return (
+    <div>
+      <h2>Trang quản lý phòng chiếu </h2>
+    </div>
+  );
+}
