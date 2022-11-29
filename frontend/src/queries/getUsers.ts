5fc3d19c6ab9f2222464906dd3e1a79849c71c6f
GreenGrass <72069705+greengrass19000@users.noreply.github.com>
2022-11-29 13:14:29 +0700

    Fixing bugs

--- frontend/src/queries/getUsers.ts
+++ frontend/src/queries/getUsers.ts
@@ -8,13 +8,12 @@ export const getUsers = async (
 ): Promise<any> => {
   let response: AxiosResponse<any>;
   try {
-    response = await axios.get(`${config.apiEndpoint}/users/pagination?page=${page}&size=${size}`, {
-    });
-  } catch (e) {
+    response = await axios.get(`${config.apiEndpoint}/users/pagination?page=${page}&size=${size}`);
+  } catch (e) { 
     return [];
   }
   return response.data.data;
 };
 
-export const usegetUsers = (page: number, size: number) =>
+export const useGetUsers = (page: number, size: number) =>
   useQuery(['getUsers'], () => getUsers(page, size));
