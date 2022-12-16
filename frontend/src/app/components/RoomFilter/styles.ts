914ce09b50d18dab0eea53b4f348b7fd56236ab2
GreenGrass <72069705+greengrass19000@users.noreply.github.com>
2022-12-16 22:06:30 +0700

    configure showtime filter

--- frontend/src/app/components/RoomFilter/styles.ts
+++ frontend/src/app/components/RoomFilter/styles.ts
@@ -4,14 +4,9 @@ import { spacing } from '@mui/system';
 import { Theme } from '@mui/material';
 
 const useStyles = makeStyles((theme: Theme) => ({
-<<<<<<< HEAD
   roomFilter: {
     maxWidth: '300px',
     marginLeft: '3em !important',
-=======
-  searchBar: {
-    maxWidth: '300px',
->>>>>>> 1a1e306 (remove export toolbar)
   },
 }));
 
--- frontend/src/app/components/SearchBar/styles.ts
+++ frontend/src/app/components/SearchBar/styles.ts
@@ -4,13 +4,9 @@ import { spacing } from '@mui/system';
 import { Theme } from '@mui/material';
 
 const useStyles = makeStyles((theme: Theme) => ({
-<<<<<<< HEAD
   searchBar: {
     marginLeft: '1em !important',
   },
-=======
-  searchBar: {},
->>>>>>> 1a1e306 (remove export toolbar)
 }));
 
 export default useStyles;
