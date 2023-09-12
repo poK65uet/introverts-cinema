901957b2b05c56b622f627c8b613e3c7f5b72502
poK65uet <20020066@vnu.edu.vn>
2023-09-12 08:40:22 +0000

    deploy nginx

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,7 +3,7 @@ export default {
 	mysql_host: process.env.MYSQL_HOST,
 	mysql_port: parseInt(process.env.MYSQL_PORT),
 	mysql_username: process.env.MYSQL_USERNAME,
-	mysql_password: process.env.MYSQL_PASSWORD,
+ 	mysql_password: process.env.MYSQL_PASSWORD,
 	mysql_database: process.env.MYSQL_DATABASE,
 	secret_key: process.env.SECRET_KEY,
 	expires_in: process.env.EXPIRESIN,
